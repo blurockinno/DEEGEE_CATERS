3295d73685a01c9ca47db2253f03cb3ade7ed613
blurockinno <152808310+blurockinno@users.noreply.github.com>
2024-05-29 10:44:57 +0530

    Update dbConnection.js
    CHANGE IN DATABASE NAME

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDatabase = () => {
   mongoose.connect(process.env.MONGODB_URL, {
-    dbName: "ERP_Solution", // MongoDB database name
+    dbName: "DEEGEE_CATERERS", // MongoDB database name
   })
     .then(() => console.log("ERP solution database connected."))
     .catch((error) => console.error("Error connecting to MongoDB:", error));
